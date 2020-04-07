c319d5b36981b1bcb2029da367db91383ce15941
Raja Junejo <junejoraja11@gmail.com>
2020-04-07 01:29:47 -0700

    Add project files.

---
+++ Healthcare.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Healthcare.web.Startup))]
+namespace Healthcare.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
